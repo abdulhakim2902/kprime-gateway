48797d009b731c5b21a6668f2e38a876510eb02b
putupadang <putupadang13@gmail.com>
2023-03-30 15:55:19 +0800

    update struct

--- internal/deribit/model/deribit.go
+++ internal/deribit/model/deribit.go
@@ -1,22 +1,22 @@
 package model
 
 type DeribitRequest struct {
-	InstrumentName string `json:"instrument_name"`
-	Amount         int    `json:"amount"`
-	Type           string `json:"type"`
-	Price          int    `json:"price"`
+	InstrumentName string  `json:"instrument_name"`
+	Amount         float64 `json:"amount"`
+	Type           string  `json:"type"`
+	Price          float64 `json:"price"`
 }
 
 type DeribitResponse struct {
-	UserId         string `json:"user_id"`
-	ClientId       string `json:"client_id"`
-	Underlying     string `json:"underlying"`
-	ExpirationDate string `json:"expiration_date"`
-	StrikePrice    string `json:"strike_price"`
-	Type           string `json:"type"`
-	Side           string `json:"side"`
-	Price          int    `json:"price"`
-	Amount         int    `json:"amount"`
+	UserId         string  `json:"user_id"`
+	ClientId       string  `json:"client_id"`
+	Underlying     string  `json:"underlying"`
+	ExpirationDate string  `json:"expiration_date"`
+	StrikePrice    string  `json:"strike_price"`
+	Type           string  `json:"type"`
+	Side           string  `json:"side"`
+	Price          float64 `json:"price"`
+	Amount         float64 `json:"amount"`
 }
 
 // type User struct {
