33b29cffa1404cc46d7597dbd2fee2cc59b823b8
firly <firly@undercurrent.tech>
2023-04-11 09:06:00 +0700

    subs market data

--- internal/ordermatch/ordermatch.go
+++ internal/ordermatch/ordermatch.go
@@ -47,6 +47,7 @@ import (
 
 var orderSubs map[string][]quickfix.SessionID
 var userSession map[string]*quickfix.SessionID
+var orderSubs map[string][]quickfix.SessionID
 
 // Application implements the quickfix.Application interface
 type Application struct {
