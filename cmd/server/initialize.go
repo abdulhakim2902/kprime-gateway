ba7007fa4ed0cc74ab6b69267f1a76d0a238dbdb
firly@undercurrent.tech <firly@undercurrent.tech>
2023-06-07 13:02:16 +0700

    init systems only

--- cmd/server/initialize.go
+++ cmd/server/initialize.go
@@ -10,11 +10,5 @@ var groupID = "gateway-group"
 
 func InitializeData(r *mongodb.Repositories, mr *memdb.Repositories) {
 	initialize := i.NewInitialize(r, mr)
-
-	// initialize
-	initialize.Price()
-	initialize.Trade()
-	initialize.Order()
 	initialize.System()
-	initialize.Activity()
 }
